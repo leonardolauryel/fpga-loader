1b9c6ef5190fa55aaf20ce117743c47e2d553667
leonardolauryel <leonardolauryel@hotmail.com>
2023-07-19 00:31:17 -0300

    Add conteinerização

--- uploadCodeToFPGA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Salvando nome do arquivo
-nameFile="$1"
-serialNumber="$2"
-
-# Enviar .bit para a FPGA
-echo "Enviando arquivo $nameFile para FPGA"
-echo "Y" | djtgcfg prog -d SN:$serialNumber -i 0 -f ./$nameFile
