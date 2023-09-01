8a792140d5e1eef21bb2907fe5579018dbd1f280
leonardolauryel <leonardolauryel@hotmail.com>
2023-09-01 07:09:14 -0300

    Melhorando logs de execução

--- fpgaSerialDataCollector/FSerDaC.py
+++ fpgaSerialDataCollector/FSerDaC.py
@@ -54,10 +54,11 @@ def getFPGASerialData():
         with open(fileName, 'w') as file:
             while (time.time() - startTime) < serialReadTime:
                 serialData = ser.readline()
-                runTime = str(time.time() - startTime)
+                runTime = time.time() - startTime
+                runTime = f"{runTime:.6f}"
                 decodedData = serialData.decode().strip()
                 logging.info(decodedData)
-                file.write(runTime + '  ' + decodedData + '\n')
+                file.write(f"[{str(runTime)}]\t\t{decodedData}\n")
                 
             logging.info("Dados coletados da serial com sucesso")
     except Exception as e:
